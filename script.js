b11b2c54790f001728d11d23fad46e90b22da321
AGbaby09 <116189737+AGbaby09@users.noreply.github.com>
2022-11-04 14:43:51 +0000

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let mT = document.getElementById('main-text');
 let sT = document.getElementById('sub-text');
+let lT = document.getElementById('list-text');
+let lTs = document.getElementById('list');
 
 mT.style.animation = 'mainText 3s linear';
 sT.style.animation = 'show 5s ease';
+lTs.style.listStyle = '';
