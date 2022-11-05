06983076c36322bc3325b518e7e345aa09390686
AGbaby09 <116189737+AGbaby09@users.noreply.github.com>
2022-11-05 12:56:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-window.onload(){
+window.onload = function() {
+    // Some code
     let mT = document.getElementById('main-text');
     let sT = document.getElementById('sub-text');
     let lT = document.getElementById('list-text');
     let cH = document.getElementById('choose');
     let fT = document.getElementById('foot');
-
+    
     mT.style.animation = 'mainText 3s linear';
     sT.style.animation = 'show 5s ease';
     cH.style.animation = 'show 5s ease';
     lT.style.animation = 'show 5s ease';
     fT.style.animation = 'bounce-in-bottom 5s ease';
- 
-}
+
+};
